c1917f3bb79f8456169bc2f880011b1762dc729c
Bruno Brugger <brunobbrugger@gmail.com>
2020-06-27 12:14:52 -0300

    Display a whole patch of grass

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"image"
 	_ "image/png"
 	"log"
+	"math/rand"
 
 	"github.com/bbrugger/ebidefender/static/sprites"
 	"github.com/hajimehoshi/ebiten"
@@ -12,23 +13,54 @@ import (
 
 const (
 	screenWidth  = 800
-	screenHeight = 600
+	screenHeight = 640
+	spriteSize   = 32
 )
 
-var grassImage *ebiten.Image
+type Sprite []byte
 
-func init() {
-	img, _, err := image.Decode(bytes.NewReader(sprites.Grass_32))
-	if err != nil {
-		log.Fatal(err)
-	}
+type Tile struct {
+	spr *ebiten.Image
+}
 
-	grassImage, _ = ebiten.NewImageFromImage(img, ebiten.FilterDefault)
+type Board struct {
+	tiles [][]Tile
 }
 
 type Game struct {
 	inited bool
 	op     ebiten.DrawImageOptions
+}
+
+var grassImages []*ebiten.Image
+var board Board
+
+func init() {
+	grassImages = make([]*ebiten.Image, 2)
+	grassSprites := []Sprite{
+		sprites.Grass_32,
+		sprites.Grass_32_2,
+	}
+
+	for i := range grassImages {
+		img, _, err := image.Decode(bytes.NewReader(grassSprites[i]))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		grassImages[i], _ = ebiten.NewImageFromImage(img, ebiten.FilterDefault)
+	}
+
+	board.tiles = make([][]Tile, screenHeight/spriteSize)
+	for i := range board.tiles {
+		board.tiles[i] = make([]Tile, screenWidth/spriteSize)
+	}
+
+	for r := range board.tiles {
+		for c := range board.tiles[r] {
+			board.tiles[r][c].spr = grassImages[rand.Intn(len(grassImages))]
+		}
+	}
 }
 
 func (g *Game) Update(screen *ebiten.Image) error {
@@ -39,7 +71,13 @@ func (g *Game) Update(screen *ebiten.Image) error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	screen.DrawImage(grassImage, &g.op)
+	for r := range board.tiles {
+		for c := range board.tiles[r] {
+			g.op.GeoM.Reset()
+			g.op.GeoM.Translate(float64(c)*spriteSize, float64(r)*spriteSize)
+			screen.DrawImage(board.tiles[r][c].spr, &g.op)
+		}
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
